3a3aff723b6b69227cb30de7946e4701363b6c38
Hossain Ahamed <hossainahamed6872@gmail.com>
2023-05-26 12:30:27 +0600

    toaster in main file -tech hub

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes/Routes.jsx'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='max-w-screen-2xl mx-auto'>
       <RouterProvider router={router}></RouterProvider>
+      <Toaster/>
     </div>
   </React.StrictMode>,
 )
