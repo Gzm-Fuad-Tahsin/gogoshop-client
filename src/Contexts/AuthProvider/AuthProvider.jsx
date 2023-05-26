9e4bf56e3ccca54016a33972127d23873984e1e2
Hossain-Ahamed <hossainahamed6872@gmail.com>
2023-05-27 00:28:03 +0600

    user routing done

--- src/Contexts/AuthProvider/AuthProvider.jsx
+++ src/Contexts/AuthProvider/AuthProvider.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     //user
-    const [user, setUser] = useState({name:'hdsjflkds'});
+    const [user, setUser] = useState();
 
     //laoding
     const [loading, setLoading] = useState(false);
