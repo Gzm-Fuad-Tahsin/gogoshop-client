801032b783d045e98393b7c06be3a9b0b052dc6b
Hossain Ahamed <hossainahamed6872@gmail.com>
2023-05-26 15:54:18 +0600

    color root added root-100 root-200 w-478-29.87rem

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "root-100" : "#4ABA6F",
+        "root-200" : "#3aad60"
+      },
+      width : {
+        '478' : '29.87rem'
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
     themes: false,
   },
-  
+
 }
 
