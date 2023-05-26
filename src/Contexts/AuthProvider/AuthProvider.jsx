2cdad5134dfaf8c76e56d2c2f59d6d80e5d00a90
Hossain-Ahamed <hossainahamed6872@gmail.com>
2023-05-26 22:11:34 +0600

    user handle added -hossain

---
+++ src/Contexts/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+
+const AuthProvider = ({ children }) => {
+    //user
+    const [user, setUser] = useState(null);
+
+    //laoding
+    const [loading, setLoading] = useState(false);
+
+
+
+
+    //value 
+    const authInfo = {
+        user,
+        loading,
+        setLoading,
+
+
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
