fd0abdfde3416870714fc45c9ec718e075c52959
HasanAhamed1 <hasanahmed95695@gmail.com>
2023-05-27 18:22:47 +0600

    table alpha1

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopNavbar from '../../Shared/TopNavBar/TopNavbar';
+import ReviewCarts from '../../ReviewCart/ReviewCarts';
 
 const Home = () => {
     return (
         <>
             <TopNavbar />
+            <ReviewCarts></ReviewCarts>
         </>
     );
 };
