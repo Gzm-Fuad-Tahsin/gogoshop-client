f407729060e4731bb1566e6830b45239cacdef80
Hossain-Ahamed <hossainahamed6872@gmail.com>
2023-05-27 15:03:25 +0600

    icon and utility context

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,23 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes/Routes.jsx'
 import { Toaster } from 'react-hot-toast'
 import AuthProvider from './Contexts/AuthProvider/AuthProvider'
+import UtilityProvider from './Contexts/Utility/UtilityProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <div className='max-w-screen-2xl mx-auto'>
-        <RouterProvider router={router}></RouterProvider>
-        <Toaster
-          position="bottom-right"
-          reverseOrder={false} />
-      </div>
-    </AuthProvider>
+    <UtilityProvider>
+
+      <AuthProvider>
+        <div className='mx-auto'>
+
+          <RouterProvider router={router}></RouterProvider>
+
+          <Toaster
+            position="bottom-right"
+            reverseOrder={false} />
+
+        </div>
+      </AuthProvider>
+    </UtilityProvider>
   </React.StrictMode>,
 )
