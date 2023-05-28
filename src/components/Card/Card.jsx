3df27ccc113a761ab2205859376477c861312fa7
HasanAhamed1 <hasanahmed95695@gmail.com>
2023-05-28 18:43:00 +0600

    table alpha3

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -11,21 +11,21 @@ const Card = ({ product }) => {
   return (
     <>
 
-      <div className="card border-2 w-[140px] h-[180px] md:w-[200px] md:h-[320px] lg:w-[230px] lg:h-[340px] 2xl:w-[230px] 2xl:h-[340px]">
+      <div className="card border-2 w-[140px] h-[180px] md:w-[200px] md:h-[320px] lg:w-[220px] lg:h-[290px] 2xl:w-[220px] 2xl:h-[290px] hover:shadow-lg">
         <Link to={`/product/${u_name}`}>
           <figure>
-            <img className="w-28 h-28 md:w-40 md:h-40 lg:w-44 lg:h-44 2xl:w-44 2xl:h-44" src={img} />
+            <img className="w-28 h-28 md:w-40 md:h-40 lg:w-38 lg:h-38 2xl:w-38 2xl:h-38 p-2" src={img} />
           </figure>
           <div className="px-3">
-            <div className="md:my-5 lg:my-5 2xl:my-5">
-              <h2 className="card-title text-[11px] md:text-xl lg:text-2xl 2xl:text-2xl font-normal">
+            <div className="md:my-5 lg:my-1 2xl:my-1">
+              <h2 className="card-title text-[11px] md:text-xl lg:text-lg 2xl:text-lg font-normal">
                 {name}
               </h2>
               <p className="text-[9px] md:text-[12px] lg:text-[13px] 2xl:text-[13px] text-[#A9A9A9]">
                 {size}
               </p>
 
-              <p className="flex text-[11px] md:text-[18px] lg:text-[20px] 2xl:text-[20px] text-[#807F7F]">
+              <p className="flex text-[11px] md:text-[18px] lg:text-[18px] 2xl:text-[18px] text-[#807F7F]">
                 <TbCurrencyTaka></TbCurrencyTaka>
                 {finalPrice}
                 {mainPrice && (
@@ -38,7 +38,7 @@ const Card = ({ product }) => {
             </div>
           </div>
         </Link>
-        <button onClick={() => handleCart(u_name)} className="btn btn-sm btn-circle bg-root-100 hover:bg-root-200 md:w-[190px] md:h-[46px]  lg:w-[220px] lg:h-[46px] 2xl:w-[220px] 2xl:h-[46px] border-0 rounded-[20px] md:font-medium md:text-[20px] lg:font-medium lg:text-[20px] 2xl:font-medium 2xl:text-[20px] normal-case absolute bottom-1 right-1 md:bottom-1 md:left-1 md:gap-2 lg:bottom-1 lg:left-1 lg:gap-2 2xl:bottom-1 2xl:left-1 2xl:gap-2">
+        <button onClick={() => handleCart(u_name)} className="btn btn-sm btn-circle bg-root-100 hover:bg-root-200 md:w-[190px] md:h-[46px]  lg:w-[208px] lg:h-[38px] 2xl:w-[208px] 2xl:h-[38px] border-0 rounded-xl md:font-medium md:text-[20px] lg:font-medium lg:text-[20px] 2xl:font-medium 2xl:text-[20px] normal-case absolute bottom-1 right-1 md:bottom-1 md:left-1 md:gap-2 lg:bottom-1 lg:left-1 lg:gap-2 2xl:bottom-1 2xl:left-1 2xl:gap-2">
           <AiOutlineShoppingCart></AiOutlineShoppingCart>
           <span className="hidden md:block lg:block 2xl:block">Add to Cart</span>
           
--- src/pages/SubCategoryProductView/SubCategoryProductView.jsx
+++ src/pages/SubCategoryProductView/SubCategoryProductView.jsx
@@ -12,7 +12,7 @@ const SubCategoryProductView = () => {
 
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 justify-items-center gap-6 w-[300px] md:w-[650px] lg:w-[1000px] 2xl:w-[1250px] m-8 mx-auto md:mx-auto lg:mx-auto 2xl:mx-auto">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 justify-items-center gap-2 md:gap-5 w-[300px] md:w-[650px] lg:w-[950px] 2xl:w-[1450px] m-8 mx-auto">
         {products.map((product) => (
           <Card key={product.id} product={product}></Card>
         ))}
