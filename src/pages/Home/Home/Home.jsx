31d890d18f92ac55337dc2079ecf6314f7fe305a
HasanAhamed1 <hasanahmed95695@gmail.com>
2023-05-29 01:03:07 +0600

    edited card

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReviewCarts from '../../ReviewCart/ReviewCarts';
+import ReviewCartDiv from '../../ReviewCart/ReviewCartDiv';
+import ReviewCartsDiv from '../../ReviewCart/ReviewCartsDiv';
 
 
 const Home = () => {
     return (
         <>
-        <ReviewCarts></ReviewCarts>
-        
+        {/* <ReviewCarts></ReviewCarts> */}
+        <ReviewCartsDiv></ReviewCartsDiv>
         </>
     );
 };
--- src/pages/SubCategoryProductView/SubCategoryProductView.jsx
+++ src/pages/SubCategoryProductView/SubCategoryProductView.jsx
@@ -12,7 +12,7 @@ const SubCategoryProductView = () => {
 
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 justify-items-center gap-2 md:gap-5 w-[300px] md:w-[650px] lg:w-[950px] 2xl:w-[1450px] m-8 mx-auto">
+      <div className="flex flex-wrap">
         {products.map((product) => (
           <Card key={product.id} product={product}></Card>
         ))}
