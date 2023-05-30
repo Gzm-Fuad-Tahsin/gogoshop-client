6d3ae2028248c3d9df2840960c9c6cdd1042e188
Hossain-Ahamed <hossainahamed6872@gmail.com>
2023-05-31 04:04:53 +0600

    mobile view and topbutton

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageTitle = ({text}) => {
     return (
-        <div className='w-full p-2 lg:pl-24 mb-4 bg-gray-200'>
+        <div className='w-full p-3 lg:pl-24 mb-4 bg-gray-200'>
             <h2>{text}</h2>
         </div>
     );
