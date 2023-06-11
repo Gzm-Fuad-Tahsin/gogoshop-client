f8dabe9a28ea092e9b3a7096f8ffb34f86f6b9b9
HasanAhamed1 <hasanahmed95695@gmail.com>
2023-06-11 22:56:14 +0600

    TopBanner-1500/600 SecondBanner-1500/500

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -13,31 +13,66 @@ import "../Banner/style.css"
 import { Autoplay, Pagination, Navigation } from "swiper";
 
 const Banner = () => {
-    return (
-        <>
-        <div className=' mt-3 flex justify-center px-3 md:px-0'>
-            <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper w-full aspect-[9/3] rounded-md md:rounded-none mx-auto"
-      >
-        <SwiperSlide><img src="https://previews.123rf.com/images/hstrongart/hstrongart1810/hstrongart181000124/110980363-mango-smoothie-banner-ads-with-splashing-beverage-pouring-into-take-out-cup-in-3d-illustration-on.jpg" alt="" className='w-full' /></SwiperSlide>
-        <SwiperSlide><img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/3a522537849839.5762514421f26.png" alt="" className='w-full' /></SwiperSlide>
-        <SwiperSlide><img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2dbe9e99759469.5efa141005230.png" alt="" className='w-full' /></SwiperSlide>
-        <SwiperSlide><img src="https://www.johnsonsbaby.in/sites/jbaby_in/files/slide-images/gentle_touch_banner_4284x1700px.jpg" alt="" className='w-full' /></SwiperSlide>
-      </Swiper>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div className=" mt-3 flex justify-center px-3 md:px-0">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          rewind={true}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper w-[520px] lg:w-[780px] xl:w-[1120px] 2xl:w-[2350px] rounded-md md:rounded-none mx-auto"
+        >
+
+          {/* image: 1500/600 */}
+          <SwiperSlide>
+            <img
+              src="https://images.unsplash.com/photo-1506617564039-2f3b650b7010?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGdyb2Nlcnl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1500&h=600"
+              alt=""
+              className="w-full "
+              loading="lazy"
+            />
+            <div class="swiper-lazy-preloader"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://images.unsplash.com/photo-1628102491629-778571d893a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z3JvY2VyeXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1500&h=600"
+              alt=""
+              className="w-full "
+              loading="lazy"
+            />
+            <div class="swiper-lazy-preloader"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1500&h=600"
+              alt=""
+              className="w-full "
+              loading="lazy"
+            />
+            <div class="swiper-lazy-preloader"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1500&h=600"
+              alt=""
+              className="w-full "
+              loading="lazy"
+            />
+            <div class="swiper-lazy-preloader"></div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
 };
 
 export default Banner;
