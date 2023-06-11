69345b1bece292b4d9b06baaf7cd9d4dce838bc8
Hossain-Ahamed <hossainahamed6872@gmail.com>
2023-06-11 23:20:10 +0600

    category image overlay

--- src/components/ImageOverlayText/ImageOverlayText.jsx
+++ src/components/ImageOverlayText/ImageOverlayText.jsx
@@ -4,12 +4,12 @@ const ImageOverlayText = ({ data }) => {
     console.log(data)
     const { category_unique_name, category_name, img } = data
     return (
-        <div className=' w-40 md:w-52 h-20 md:h-24 rounded-md hover:shadow-lg m-2  re'>
+        <div className=' w-40 md:w-52 h-20 md:h-24 rounded-md hover:shadow-lg m-2  relative '>
             
 
-                <img src='https://source.unsplash.com/HkTMcmlMOUQ' className=' w-40 md:w-52 h-20 md:h-24 rounded-md' alt="" />
+                <img src='https://source.unsplash.com/HkTMcmlMOUQ' className=' w-40 md:w-52 h-20 md:h-24 rounded-md ' alt="" />
            
-            <div className="boya">
+            <div className="absolute -translate-y-14 translate-x-4 text-white text-2xl font-medium">
                 <h2>dfslkfjsdj</h2>
             </div>
         </div>
